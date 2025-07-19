e3d33350103035af5e78b87d5038e6ddc41383d2
Z Force <zforce8827@gmail.com>
2025-07-19 22:44:29 +0000

    import type { NextConfig } from 'next';
    const nextConfig: NextConfig =

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type {NextConfig} from 'next';
+
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -13,7 +13,6 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'placehold.co',
-        port: '',
         pathname: '/**',
       },
     ],
